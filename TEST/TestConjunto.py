6cd0fa4a504a7f9845234367cf46dd850cdaba17
labf402 <70582220@continental.edu.pe>
2024-10-15 16:18:27 -0500

    CASO 1 Calcular promedio con conjunto vacio

--- TEST/TestConjunto.py
+++ TEST/TestConjunto.py
@@ -1,5 +1,6 @@
 import unittest
 from SRC.LOGICA.Conjunto import Conjunto
-
 class TestConjunto( unittest.TestCase ):
-    pass
+    def test_conjunto_vacio_retornarnone(self):
+        conjunto = Conjunto([])
+        self.assertEqual()
